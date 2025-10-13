9cc3ecbb5d20e6e963788d96dfb9f978cec3c5db
Ricardo Fernandes <151522735+FernandeeSxS@users.noreply.github.com>
2025-10-13 18:09:00 +0100

    Implementação de autenticação automática e correção do Join

--- src/TabelaAnaliseCompleta.sql
+++ src/TabelaAnaliseCompleta.sql
@@ -1,12 +1,14 @@
 CREATE TABLE Tabela_Analise_Completa (
-    ID_Musica VARCHAR(50) PRIMARY KEY, 
+    ID_Musica VARCHAR(50) NOT NULL, 
     Nome_Musica VARCHAR(255) NOT NULL,
     Nome_Album VARCHAR(255) NULL,
     Duracao_Min NVARCHAR(10) NULL,
     Popularidade_Musica INT NULL,
 
-    Nome_Artista VARCHAR(500) NULL,
-    Generos_Musicais VARCHAR(500) NULL, 
+    Nome_Artista VARCHAR(500) NOT NULL, 
+    Generos_Musicais VARCHAR(500) NULL, 
     Popularidade_Artista INT NULL,
-    Seguidores BIGINT NULL
+    Seguidores BIGINT NULL,
+
+    PRIMARY KEY (ID_Musica, Nome_Artista)
 );
